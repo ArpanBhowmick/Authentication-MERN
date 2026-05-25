f474510398b6000ddc6fc9061c77e81706f68681
ArpanBhowmick <arpanbhowmick28@gmail.com>
2026-05-25 20:14:22 +0530

    package name fixed

--- Backend/server.js
+++ Backend/server.js
@@ -4,20 +4,20 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import corsOptions from "./config/corsOptions.js";
-import cookiesParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 app.use(cors(corsOptions));
-app.use(cookiesParser());
+app.use(cookieParser());
 app.use(express.json());
 
 
+await connectDB();
+
 app.use("/api/auth", authRoute);
 
-await connectDB();
 
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
